20e0962c7b70274fb72047f8658bb9cc6afb5d17
Jaan Taponen <jaan.taponen@awake.ai>
2021-12-11 17:08:01 +0200

    Implement fifo calculator, add jest, babel, typescript

---
+++ src/fifo/index.ts
@@ -0,0 +1,13 @@
+import {
+  calculateFIFOCapitalGains,
+  CapitalResults,
+  calculateFIFOTransactions,
+} from './fifo';
+import { Operation } from './types';
+
+export {
+  calculateFIFOCapitalGains,
+  CapitalResults,
+  calculateFIFOTransactions,
+  Operation,
+};
