c2e308b1e69dfd622e6016a8b05b0bcb9a22e407
JaanTaponen <jaan.taponen@hotmail.com>
2021-12-13 20:04:20 +0200

    refactor crypto into separate page

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -5,13 +5,15 @@ import { Dropzone } from './components/Dropzone'
 import FrontPage from './components/Frontpage'
 import { Securities } from './components/Securities'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Crypto } from './components/Crypto'
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<FrontPage />} />
-      <Route path="maol" element={<Dropzone handleFiles={() => console.log("hyvä elämä")}zoneHeight={400}/>} />
+      <Route path="maol" element={<Dropzone handleFiles={() => console.log("hyvä elämä")} zoneHeight={400} />} />
       <Route path="securities" element={<Securities />} />
+      <Route path="crypto" element={<Crypto />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
