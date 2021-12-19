e5566dcb4cf68de934956c27f558fff9384aa1d1
JaanTaponen <jaan.taponen@hotmail.com>
2021-12-19 14:39:18 +0200

    allo plain css files jest

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,10 @@ export default {
   // cacheDirectory: "/tmp/jest_rs",
 
   // Automatically clear mock calls and instances between every test
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(scss|sass|css)$": "identity-obj-proxy"
+  },
   clearMocks: true,
   testPathIgnorePatterns: ["./cypress/"],
 
